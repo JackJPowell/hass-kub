2f2666c716aec6fb870b5688d4c46e753bbe1530
Jack Powell <jackattack51@gmail.com>
2024-06-08 12:40:51 +0000

    Support waste water

--- custom_components/kub/const.py
+++ custom_components/kub/const.py
@@ -5,5 +5,6 @@ from datetime import timedelta
 DOMAIN = "kub"
 DEVICE_SCAN_INTERVAL = timedelta(hours=12)
 KUB_COORDINATOR = "kub_coordinator"
+CONF_WATER_STATISTICS = "water_statistics"
 KUB_API = "kub_api"
 KUB_USER = "kub_user"
